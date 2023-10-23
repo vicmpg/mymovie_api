f87f8adc50cfd78da5ff8935aa2ba5ea1d14a036
vicmpg <vicmpg22@gmail.com>
2023-10-23 16:08:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to MyFlix');
 });
 
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
