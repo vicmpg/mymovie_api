b5ef73dcae014052d7ba59766f2145cc3eef8b08
vicmpg <vicmpg22@gmail.com>
2024-03-11 20:41:08 -0400

    Added local Host 4200

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:4200'];
 
 
 app.use(cors({
