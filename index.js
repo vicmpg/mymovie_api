4c35c56d212b28ad3abdba8fdf6d4e7bf24aff06
vicmpg <vicmpg22@gmail.com>
2024-03-11 21:43:46 -0400

    Addind deployed Git page

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:4200'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:4200','https://vicmpg.github.io'];
 
 
 app.use(cors({
