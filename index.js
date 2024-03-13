6c86c2099f17b24f7fe63943cce314d54f2eadde
vicmpg <vicmpg22@gmail.com>
2024-03-13 16:15:21 -0400

    Fixing CORS erros

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:4200','https://vicmpg.github.io/myFlix-Angular-client/', 'https://vicmpg.github.io'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:4200','https://vicmpg.github.io/myFlix-Angular-client/welcome', 'https://vicmpg.github.io'];
 
 
 app.use(cors({
