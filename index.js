08e32ffb1425a5ec37b7d291b189f87aa59706a6
vicmpg <vicmpg22@gmail.com>
2024-04-08 11:31:41 -0400

    Adding CORS for netlify

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:4200', 'https://vicmpg.github.io', 'http://localhost:50003'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:4200', 'https://vicmpg.github.io', 'http://localhost:50003', 'https://euphonious-frangipane-9f896b.netlify.app'];
 
 
 app.use(cors({
